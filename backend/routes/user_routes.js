fef9730b3bd907349abfcdb1f44d4023624edccb
kripa1205 <panchalkripa0@gmail.com>
2025-07-03 16:57:54 +0530

    Update user_routes.js

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,14 +1,22 @@
-const express= require('express')
-const { newuser, userget, edituser, deleteuser,login, Authverify } = require('../controllers/user_controller')
-const Auth = require('../middleware/auth')
-const router = express.Router()
+const express = require("express");
+const {
+  newuser,
+  userget,
+  edituser,
+  deleteuser,
+  login,
+  Authverify,
+  verifyEmail,
+} = require("../controllers/user_controller");
+const Auth = require("../middleware/auth");
+const router = express.Router();
 
-
-router.post('/newuser', newuser)
-router.get('/getuser', userget)
-router.put('/edituser/:id', edituser);
-router.delete('/deleteuser/:id', deleteuser)
-router.post('/login',login)
-router.post('/authverify',Auth,Authverify)
+router.post("/newuser", newuser);
+router.get("/getuser", userget);
+router.put("/edituser/:id", edituser);
+router.delete("/deleteuser/:id", deleteuser);
+router.post("/login", login);
+router.post("/authverify", Auth, Authverify);
+router.get("/verify/:token", verifyEmail); 
 
 module.exports = router;
